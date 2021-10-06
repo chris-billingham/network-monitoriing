09e26f34318d40b657f5de649b6f2abab5a17bcf
chris-billingham <chris.billingham@gmail.com>
2021-10-06 14:35:15 +0100

    sorting and tidying the speedtest data

--- R/01_internet-speedtest.R
+++ R/01_internet-speedtest.R
@@ -1,3 +1,5 @@
+library(magrittr)
+
 # run a speedtest
 logger::log_info("running speedtest")
 test <- system("/home/pi/.local/bin/speedtest", intern = TRUE)
@@ -14,23 +16,28 @@ conv_to_bits <- function(bandwidth) {
 # create the update tibble
 logger::log_info("creating speedtest dataframe")
 st_df <- tibble::tibble(test_datetime = Sys.time(),
-                        isp = test[2],
-                        test_server = test[5],
+                        isp_name = stringr::str_extract(test[2], "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}"),
+                        isp_ip = stringr::str_match(test[2], "Testing from (.*?) \\(")[2],
+                        test_server_name = stringr::str_match(test[5], "Hosted by (.*?) \\(")[2],
+                        test_server_location = stringr::str_match(test[5], "\\((.*?)\\)")[2],
+                        test_server_distance_km = as.numeric(stringr::str_extract(stringr::str_match(test[5], "\\[(.*?)\\]")[2],"[0-9\\.]+")),
+                        test_server_ping_ms = as.numeric(stringr::str_extract(stringr::str_match(test[5], ": (.*?)$")[2],"[0-9\\.]+")),
                         download = test[7],
                         download_bits = conv_to_bits(test[7]),
                         upload = test[9],
-                        upload_bits = conv_to_bits(test[9]))
+                        upload_bits = conv_to_bits(test[9])) %>%
+  dplyr::select(-download, -upload)
 
 # load the old data
 logger::log_info("reading old data")
-old <- readRDS("data/speedtest.rds")
+old <- readRDS("/home/pi/nas-share/R/network-monitoring/data/speedtest.rds")
 
 # append together
 new <- dplyr::bind_rows(old, st_df)
 
 # save data
 logger::log_info("saving with new data")
-saveRDS(new, "data/speedtest.rds")
+saveRDS(new, "/home/pi/nas-share/R/network-monitoring/data/speedtest.rds")
 
 # fin
 logger::log_success("#fin")
